c17fb449f6f486988a551a17c0f66b81b50a9e77
MehranMohseni <mrnmsni@gmail.com>
2022-06-05 00:29:19 +0430

    feat: Added exception handling + response status code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, Body, Depends
-import schemas
-import models
+from fastapi import Body, Depends, FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
 
-from database import Base, engine, SessionLocal
-from sqlalchemy.orm import Session 
+import models
+import schemas
+from database import Base, SessionLocal, engine
 
 Base.metadata.create_all(engine)
+
 
 def get_session():
     session = SessionLocal()
@@ -14,42 +16,47 @@ def get_session():
     finally:
         session.close()
 
+
 app = FastAPI()
 
 fakeDatabase = {
-    1:{'task':'Clean car'},
-    2:{'task':'Write blog'},
-    3:{'task':'Start stream'},
+    1: {'task': 'Clean car'},
+    2: {'task': 'Write blog'},
+    3: {'task': 'Start stream'},
 }
 
-@app.get("/")
+# Different way using "status"
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
 def getItems(session: Session = Depends(get_session)):
-    items = session.query(models.Item).all()
-    return items
+    return session.query(models.Item).all()
 
-@app.get("/{id}")
-def getItem(id:int, session: Session = Depends(get_session)):
-    item = session.query(models.Item).get(id)
-    return item
 
-#option #1
+@app.get("/{id}", status_code=200)
+def getItem(id: int, session: Session = Depends(get_session)):
+    return session.query(models.Item).get(id)
+
+# option #1
 # @app.post("/")
 # def addItem(task:str):
 #     newId = len(fakeDatabase.keys()) + 1
 #     fakeDatabase[newId] = {"task":task}
 #     return fakeDatabase
 
-#Option #2
-@app.post("/")
-def addItem(item:schemas.Item, session: Session = Depends(get_session)):
-    item = models.Item(task = item.task)
+# Option #2
+
+
+@app.post("/", status_code=201)
+def addItem(item: schemas.Item, session: Session = Depends(get_session)):
+    item = models.Item(task=item.task)
     session.add(item)
     session.commit()
     session.refresh(item)
 
     return item
 
-#Option #3
+# Option #3
 # @app.post("/")
 # def addItem(body = Body()):
 #     newId = len(fakeDatabase.keys()) + 1
@@ -57,8 +64,8 @@ def addItem(item:schemas.Item, session: Session = Depends(get_session)):
 #     return fakeDatabase
 
 
-@app.put("/{id}")
-def updateItem(id:int, item:schemas.Item, session: Session = Depends(get_session)):
+@app.put("/{id}", status_code=200)
+def updateItem(id: int, item: schemas.Item, session: Session = Depends(get_session)):
     itemObject = session.query(models.Item).get(id)
     itemObject.task = item.task
     session.commit()
@@ -66,9 +73,16 @@ def updateItem(id:int, item:schemas.Item, session: Session = Depends(get_session
 
 
 @app.delete("/{id}")
-def deleteItem(id:int, session: Session = Depends(get_session)):
-    itemObject = session.query(models.Item).get(id)
-    session.delete(itemObject)
-    session.commit()
-    session.close()
-    return 'Item was deleted...'
+def deleteItem(id: int, session: Session = Depends(get_session)):
+    try:
+        itemObject = session.query(models.Item).get(id)
+        session.delete(itemObject)
+        session.commit()
+        session.close()
+    except Exception:
+        # You could also use "HTTPException"
+        return JSONResponse(
+            status_code=404,
+            content={
+                "message": "Item Not Found"},
+        )
